c6c2cb4215925ff25cea9648059a72d76b9ba1e5
Alex Tu <alex.tu@canonical.com>
2017-05-14 02:18:41 +0800

    add script to release

---
+++ tools/release.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -x 
+set -e
+usage() {
+cat << EOF
+usage: $0 options
+
+    -h|--help print this message
+    --remote  set the remote target
+    
+EOF
+exit 1
+}
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -h | --help)
+            usage 0
+            exit 0
+            ;;
+#        --dry-run)
+#            DRY_RUN=1;
+#            ;;
+        --remote)
+            shift
+            REMOTE=$1
+            ;;
+        *)
+        usage
+       esac
+       shift
+done
+
+# push backbone
+git push $REMOTE backbone
+# push posts
+git push $REMOTE posts
+# gh_pages merge backbone, then merge posts
+git branch tmp gh-pages
+git checkout tmp
+git merge --no-ff $REMOTE/backbone
+git merge --no-ff $REMOTE/posts
+git push $REMOTE gh-pages
+
